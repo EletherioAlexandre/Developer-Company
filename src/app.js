89e3415ef18d01199415f8f5cf2c057eb4692ac1
EletherioAlexandre <eletherioalexandre@gmail.com>
2022-04-11 21:21:14 -0300

    Modal applied

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const openModalLogin = () => {
+  const OPEN_LOGIN = document.getElementById("open-login");
+  const MODAL_CONTAINER = document.getElementById("modal-container1");
+  const CLOSE_LOGIN = document.getElementById("close-login");
+
+  OPEN_LOGIN.addEventListener("click", () => {
+    MODAL_CONTAINER.classList.add("show");
+  });
+
+  CLOSE_LOGIN.addEventListener("click", () => {
+    MODAL_CONTAINER.classList.remove("show");
+  });
+};
+
+const openModalOrcamento = () => {
+  const OPEN_ORCAMENTO = document.getElementById("open-orcamento");
+  const MODAL_CONTAINER_ORCAMENTO = document.getElementById(
+    "modal-container-orcamento"
+  );
+  const CLOSE_ORCAMENTO = document.getElementById("close-orcamento");
+
+  OPEN_ORCAMENTO.addEventListener("click", () => {
+    MODAL_CONTAINER_ORCAMENTO.classList.add("showModalOrcamento");
+  });
+
+  CLOSE_ORCAMENTO.addEventListener("click", () => {
+    MODAL_CONTAINER_ORCAMENTO.classList.remove("showModalOrcamento");
+  });
+};
