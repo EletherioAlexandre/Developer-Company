00ff666b00b30a2918cc3dc5ed2672b06c737792
EletherioAlexandre <eletherioalexandre@gmail.com>
2022-04-15 01:00:41 -0300

    Alert modals added

--- src/main-page/app.js
+++ src/main-page/app.js
@@ -28,3 +28,31 @@ const openModalOrcamento = () => {
     MODAL_CONTAINER_ORCAMENTO.classList.remove("showModalOrcamento");
   });
 };
+
+const TRIGGER_POPUP = () => {
+  const OPEN_POPUP = document.getElementById("triggerpopup");
+  const POPUP = document.getElementById("popup-container");
+  const DISMISS = document.getElementById("dismiss-popup");
+
+  OPEN_POPUP.addEventListener("click", () => {
+    POPUP.classList.add("open");
+  });
+
+  DISMISS.addEventListener("click", () => {
+    POPUP.classList.remove("open");
+  });
+};
+
+const CALL_USER = () => {
+  const BUTTON_CALL = document.getElementById("liveToastBtn");
+  const SUCCESS_POPUP = document.getElementById("popup-ligacao");
+  const HIDE_POPUP = document.getElementById("popup-ligacao");
+
+  BUTTON_CALL.addEventListener("click", () => {
+    SUCCESS_POPUP.classList.add("send");
+  });
+
+  HIDE_POPUP.addEventListener("mouseover", () => {
+    SUCCESS_POPUP.classList.remove("send");
+  });
+};
